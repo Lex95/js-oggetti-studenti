def03b5900ff377fa75695ddae3a8d22701aeba8
Lex95 <andrea.lega1995@gmail.com>
2021-04-22 15:32:43 +0200

    risoluzione seconda parte

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ for (key in studente) {
 }
 
 // seconda parte
+var studenti = [
+    {
+        nome: "Gianni",
+        cognome: "Sperti",
+        eta: 20
+    },
+    {
+        nome: "Lucenzo",
+        cognome: "Stello",
+        eta: 26
+    },
+    {
+        nome: "Maurilio",
+        cognome: "Efesto",
+        eta: 34
+    }
+];
+for (var i = 0; i < studenti.length; i++) {
+    for (arrKey in studenti[i]) {
+        document.getElementById("myP2").innerHTML += arrKey + ": " + studenti[i][arrKey] + "<br>";
+    }
+}

047ce1b68b83b2d243ce8d732194475708de86ec
Lex95 <andrea.lega1995@gmail.com>
2021-04-22 15:39:20 +0200

    risoluzione terza parte

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var studente = {
     nome: "Andrea",
     cognome: "Lega",
     eta: 26
-}
+};
 for (key in studente) {
     document.getElementById("myP1").innerHTML += key + ": " + studente[key] + "<br>";
-}
+};
 
 // seconda parte
 var studenti = [
@@ -30,4 +30,20 @@ for (var i = 0; i < studenti.length; i++) {
     for (arrKey in studenti[i]) {
         document.getElementById("myP2").innerHTML += arrKey + ": " + studenti[i][arrKey] + "<br>";
     }
-}
+};
+
+// terza parte
+var tempStudente = {
+    nome: "",
+    cognome: "",
+    eta: 0
+};
+for (addKey in tempStudente) {
+    tempStudente[addKey] = prompt("Inserire " + addKey + " del nuovo studente:");
+};
+studenti.push(tempStudente);
+for (var i = 0; i < studenti.length; i++) {
+    for (arrKey in studenti[i]) {
+        document.getElementById("myP3").innerHTML += arrKey + ": " + studenti[i][arrKey] + "<br>";
+    }
+};
